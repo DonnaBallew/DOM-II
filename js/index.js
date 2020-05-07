f5cab23db51e47e47aa89148bfa4434dc4f93061
Donna Ballew <ballewdd@gmail.com>
2020-05-06 19:43:46 -0700

    Event 1 & 2 - Key up and Key down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//Event 1 & 2 - KEY UP & KEY DOWN
+const newText = document.querySelector(".logo-heading");
+
+window.addEventListener("keydown", () => {
+  newText.textContent = "Come Join Us!";
+  newText.style.color = "#c7624c";
+  newText.style.fontSize = "5rem";
+});
+
+window.addEventListener("keyup", () => {
+  newText.textContent = "Fun Bus";
+  newText.style.color = "#212529";
+  newText.style.fontSize = "4rem";
+});
